ccb62d198c96bfc18c77abec0e43fd8b1733b767
manfeibai <manfei@google.com>
2023-03-28 23:53:50 +0000

    only vlp

--- test/pjrt/test_experimental_pjrt_tpu.py
+++ test/pjrt/test_experimental_pjrt_tpu.py
@@ -42,9 +42,6 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
     os.environ.pop(xenv.TPU_VISIBLE_CHIPS, None)
     os.environ.pop(xenv.TPU_PROCESS_BOUNDS, None)
 
-  @absltest.skipIf(
-      tpu.version() <= 3,
-      'This test is not currently supported on v3 TPUVMs or earlier.')
   def test_xla_devices_multiprocess(self):
     accelerator_devices = {
         'v4-8': {
